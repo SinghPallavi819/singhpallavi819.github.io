ac8959442d7e0e0a6d2f39cea96a16d0b3376f23
Pallavi <pallavi@Pallavis-MacBook-Air.local>
2024-05-09 12:54:41 -0400

    programming jokes again

--- projects/joke-teller/script.js
+++ projects/joke-teller/script.js
@@ -128,7 +128,7 @@ function tellMe(joke){
 // Get Jokes from Joke API
 async function getJokes(){
     let joke = '';
-    const apiUrl = 'https://v2.jokeapi.dev/joke/Dark?blacklistFlags=nsfw,political&type=single';
+    const apiUrl = 'https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit';
     try {
         const response = await fetch(apiUrl);
         const data = await response.json();
