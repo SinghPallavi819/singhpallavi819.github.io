eb09d495e1b024f74cb3e4329b74494b020f6cf6
Pallavi <pallavi@Pallavis-Air.attlocal.net>
2024-07-19 14:23:56 -0700

    audio updated

--- projects/whack-zombies/script.js
+++ projects/whack-zombies/script.js
@@ -4,6 +4,7 @@ let score = 0;
 let gameOver = false;
 let plantInterval;
 let zombieInterval;
+let gameOverAudio = document.getElementById("gameOverSound"); // Get reference to game over audio
 
 window.onload = function() {
     initializeBoard();
@@ -32,6 +33,7 @@ function resetGame() {
     currPlantTile = null;
     currZomTile = null;
     clearTiles();
+    gameOverAudio.pause(); // Pause game over sound if it's still playing
 }
 
 function clearTiles() {
@@ -61,6 +63,9 @@ function setPlant() {
     }
     currPlantTile = document.getElementById(num);
     currPlantTile.appendChild(plant);
+
+    // Play plant sound
+    playPlantSound();
 }
 
 function setZombie() {
@@ -79,6 +84,9 @@ function setZombie() {
     }
     currZomTile = document.getElementById(num);
     currZomTile.appendChild(zombie);
+
+    // Play zombie sound
+    playZombieSound();
 }
 
 function selectTile() {
@@ -88,12 +96,45 @@ function selectTile() {
     if (this == currPlantTile) {
         score += 10;
         document.getElementById("score").innerText = score.toString();
+        // Play plant whack sound
+        playPlantWhackSound();
     } else if (this == currZomTile) {
         document.getElementById("score").innerText = "GAME OVER " + score.toString();
         gameOver = true;
         clearInterval(plantInterval);
         clearInterval(zombieInterval);
+        // Play game over sound
+        playGameOverSound();
         document.getElementById('start').innerText = 'Start Over';
         document.getElementById('start').style.display = 'inline-block';
     }
+}
+
+// Function to play plant sound
+function playPlantSound() {
+    var sound = document.getElementById("plantSound");
+    sound.currentTime = 0; // Rewind to the start
+    sound.play();
+}
+
+// Function to play zombie sound
+function playZombieSound() {
+    var sound = document.getElementById("zombieSound");
+    sound.currentTime = 0; // Rewind to the start
+    sound.play();
+}
+
+// Function to play plant whack sound
+function playPlantWhackSound() {
+    // Example: Play a short whack sound for plant
+    // Adjust based on your sound file
+    var sound = new Audio('./sounds/plant_whack.mp3');
+    sound.volume = 0.5; // Example volume adjustment
+    sound.play();
+}
+
+// Function to play game over sound
+function playGameOverSound() {
+    gameOverAudio.currentTime = 0; // Rewind to the start
+    gameOverAudio.play();
 }
