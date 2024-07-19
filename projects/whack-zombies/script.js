27385058fbc78abee12417ac016bbb7c3ce8c5c5
Pallavi <pallavi@Pallavis-Air.attlocal.net>
2024-07-19 13:54:34 -0700

    game updated

--- projects/whack-zombies/script.js
+++ projects/whack-zombies/script.js
@@ -1,58 +1,80 @@
-
 let currPlantTile;
 let currZomTile;
 let score = 0;
 let gameOver = false;
-
+let plantInterval;
+let zombieInterval;
 
 window.onload = function() {
-    setGame();
+    initializeBoard();
 }
-function setGame() {
-    for (let i = 0 ; i < 9 ; i++){
+
+function initializeBoard() {
+    for (let i = 0; i < 9; i++) {
         let tile = document.createElement("div");
         tile.id = i.toString();
         tile.addEventListener("click", selectTile);
         document.getElementById("board").appendChild(tile);
     }
-
-    setInterval(setPlant, 1000);
-    setInterval(setZombie, 2000);
 }
-function getRandomTile (){
+
+function startGame() {
+    document.getElementById('start').style.display = 'none';
+    resetGame();
+    plantInterval = setInterval(setPlant, 1000);
+    zombieInterval = setInterval(setZombie, 2000);
+}
+
+function resetGame() {
+    score = 0;
+    gameOver = false;
+    document.getElementById("score").innerText = score.toString();
+    currPlantTile = null;
+    currZomTile = null;
+    clearTiles();
+}
+
+function clearTiles() {
+    for (let i = 0; i < 9; i++) {
+        document.getElementById(i.toString()).innerHTML = "";
+    }
+}
+
+function getRandomTile() {
     let num = Math.floor(Math.random() * 9);
     return num.toString();
 }
+
 function setPlant() {
-if (gameOver){
-    return;
-}
-    if (currPlantTile){
+    if (gameOver) {
+        return;
+    }
+    if (currPlantTile) {
         currPlantTile.innerHTML = "";
     }
     let plant = document.createElement("img");
     plant.src = "./images/flower.webp";
 
     let num = getRandomTile();
-    if (currZomTile && currZomTile.id == num){
-    return;
-}
+    if (currZomTile && currZomTile.id == num) {
+        return;
+    }
     currPlantTile = document.getElementById(num);
     currPlantTile.appendChild(plant);
 }
 
-function setZombie(){
-    if (gameOver){
+function setZombie() {
+    if (gameOver) {
         return;
     }
-    if(currZomTile){
+    if (currZomTile) {
         currZomTile.innerHTML = "";
     }
     let zombie = document.createElement("img");
     zombie.src = "./images/zombie.png";
 
     let num = getRandomTile();
-    if (currPlantTile && currPlantTile.id == num){
+    if (currPlantTile && currPlantTile.id == num) {
         return;
     }
     currZomTile = document.getElementById(num);
@@ -60,14 +82,18 @@ function setZombie(){
 }
 
 function selectTile() {
-    if (gameOver){
+    if (gameOver) {
         return;
     }
-    if(this == currPlantTile){
+    if (this == currPlantTile) {
         score += 10;
         document.getElementById("score").innerText = score.toString();
-    }else if (this == currZomTile){
+    } else if (this == currZomTile) {
         document.getElementById("score").innerText = "GAME OVER " + score.toString();
         gameOver = true;
+        clearInterval(plantInterval);
+        clearInterval(zombieInterval);
+        document.getElementById('start').innerText = 'Start Over';
+        document.getElementById('start').style.display = 'inline-block';
     }
 }
